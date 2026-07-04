627c0ed4ba852a2da695e78666c73a6b19ae6ff6
ZK-Tshz <murilo.bap.oliv@gmail.com>
2026-07-03 21:07:02 -0300

    Criando Projeto

---
+++ src/view/Menu.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.Scanner;
+
+public class Menu {
+    public void interacaoUsuario() {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o CEP que deseja buscar: ");
+        String cep = leitor.nextLine();
+
+    }
+}
